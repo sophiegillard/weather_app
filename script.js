dd7857d9e738ad1d0de6e08bbf7c0b5b074ad58a
Sophie Gillard <sophie.x.gillard@gmail.com>
2022-08-19 15:20:18 +0200

    cards are created. Need to put the inputs now for week

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const searchFilter = document.getElementById('search__filter');
     //Other variales
 let cardDiv;
 let forecast__hourly__cards;
+let forecast__weekly__cards;
+let card__content;
 
 //FUnction to get new value each time -- doesn't work !!!
 // searchFilter.addEventListener('keyup', getCityName=> {
@@ -27,12 +29,15 @@ const fetchForecast = async (url) => {
     //calling all functions to create the cards
     mainCard(city)
     headerCard(result.city.name, result.list[0].main.temp, result.list[0].weather[0].description)
-    contentCard()
+    contentCard(city)
+    contentCardHour(city)
+    contentCardWeek(city)
     console.log(result)
     //Creating the hourly weather cards
     for (let i = 0, n = 3; i < 5; i++, n+=3) {
         hourlyCard(n, `http://openweathermap.org/img/wn/${result.list[0].weather[0].icon}@2x.png`, result.list[i].main.temp)
     }
+    weeklyCard(city, `http://openweathermap.org/img/wn/${result.list[0].weather[0].icon}@2x.png`, 'Mon', 'Cloudy', 'maxDegree', 'minDegree')
 
     } catch (e) {
         alert('ERROR : please enter a valid a city in English')
@@ -74,15 +79,20 @@ const headerCard = (cityName, cityDegree, cityWeather) =>{
     forecast__actual__weather.innerHTML = cityWeather;
 }
  //Creating card content but not the small weather cards
-const contentCard = () => {
+const contentCard = (cityName) => {
     let card__content = document.createElement('section');
     card__content.classList.add('card__content');
+    card__content.classList.add(cityName);
     cardDiv.appendChild(card__content)
+}
 
-    //HOURS CARDS
+//HOURS CARDS
+const contentCardHour = (cityName) => {
+    let card__content__city =document.getElementsByClassName(`card__content ${cityName}`)[0]
+    console.log(card__content__city)
     let forecast__hourly = document.createElement('div');
     forecast__hourly.classList.add('forecast__hourly');
-    card__content.appendChild(forecast__hourly);
+    card__content__city.appendChild(forecast__hourly);
 
     let forecast__hourly__title = document.createElement('h3');
     forecast__hourly__title.classList.add('forecast__hourly__title');
@@ -92,23 +102,27 @@ const contentCard = () => {
     forecast__hourly__cards = document.createElement('div');
     forecast__hourly__cards.classList.add('forecast__hourly__cards');
     forecast__hourly.appendChild(forecast__hourly__cards);
-    return forecast__hourly__cards;
+}
 
-    //WEEKLY CARDS
+    // WEEKLY CARDS
+const contentCardWeek = (cityName) => {
+    let card__content__city =document.getElementsByClassName(`card__content ${cityName}`)[0]
     let forecast__weekly = document.createElement('div');
     forecast__weekly.classList.add('forecast__weekly');
-    card__content.appendChild(forecast__weekly);
+    card__content__city.appendChild(forecast__weekly);
 
     let forecast__weekly__title = document.createElement('h3');
     forecast__weekly__title.classList.add('forecast__weekly__title');
     forecast__weekly.appendChild(forecast__weekly__title);
     forecast__weekly__title.innerHTML="Weekly Forecast"
 
-    let forecast__weekly__card = document.createElement('div');
-    forecast__weekly__card.classList.add('forecast__weekly__card');
-    forecast__weekly.appendChild(forecast__weekly__card);
+    let forecast__weekly__cards = document.createElement('div');
+    forecast__weekly__cards.classList.add('forecast__weekly__cards');
+    forecast__weekly__cards.classList.add(cityName);
+    forecast__weekly.appendChild(forecast__weekly__cards);
 }
 
+    //HOURLY CARDS
 const hourlyCard = (hours, icon, futureDegree) =>{
     let hourly__card = document.createElement('div');
     hourly__card.classList.add('hourly__card');
@@ -128,4 +142,38 @@ const hourlyCard = (hours, icon, futureDegree) =>{
     hourly__card__degree.classList.add('hourly__card__degree');
     hourly__card.appendChild(hourly__card__degree);
     hourly__card__degree.innerHTML = futureDegree +"°C"
+}
+
+       //WEEKLY CARDS
+const weeklyCard = (cityName, icon, day, weather, maxDegree, minDegree) => {
+    let forecast__weekly__cards= document.getElementsByClassName(`forecast__weekly__cards ${cityName}`)[0]
+    let weekly__card = document.createElement('div');
+    weekly__card.classList.add('weekly__card');
+    forecast__weekly__cards.appendChild(weekly__card);
+    
+
+    let weekly__card__icon = document.createElement('img');
+    weekly__card__icon.classList.add('hourly__card__icon');
+    weekly__card.appendChild(weekly__card__icon);
+    weekly__card__icon.src= icon;
+
+    let weekly__card__time = document.createElement('p');
+    weekly__card__time.classList.add('weekly__card__time');
+    weekly__card.appendChild(weekly__card__time);
+    weekly__card__time.innerHTML = day;
+
+    let weekly__card__weather = document.createElement('p');
+    weekly__card__weather.classList.add('weekly__card__weather');
+    weekly__card.appendChild(weekly__card__weather);
+    weekly__card__weather.innerHTML = weather;
+
+    let weekly__card__maxDegree = document.createElement('p');
+    weekly__card__maxDegree.classList.add('weekly__card__maxDegree');
+    weekly__card.appendChild(weekly__card__maxDegree);
+    weekly__card__maxDegree.innerHTML = maxDegree;
+
+    let weekly__card__minDegree = document.createElement('p');
+    weekly__card__minDegree.classList.add('weekly__card__minDegree');
+    weekly__card.appendChild(weekly__card__minDegree);
+    weekly__card__minDegree.innerHTML = minDegree;
 }
